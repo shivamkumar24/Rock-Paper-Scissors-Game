ce8b63781db221940a920049d38336bf8400cb47
Shivam <shivam2432000@gmail.com>
2023-04-05 12:29:33 +0530

    This is functionality done for this game

---
+++ index.js
@@ -0,0 +1,91 @@
+let userScore = 0;
+let computerScore = 0;
+
+const userSpanScore = document.getElementById("user-score");
+const computerSpanScore = document.getElementById("computer-score");
+
+const scoreBoard = document.querySelector(".score-board");
+const result = document.querySelector(".results>p");
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choice[randomNumber];
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      win(userChoice, computerChoice);
+      break;
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      lose(userChoice, computerChoice);
+      break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      draw(userChoice, computerChoice);
+      break;
+    default:
+      break;
+  }
+}
+
+function convertLetter(letter) {
+  if (letter === "rock") return "Rock";
+  if (letter === "paper") return "Paper";
+  return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userSpanScore.innerHTML = userScore;
+  computerSpanScore.innerHTML = computerScore;
+  result.innerHTML = `${convertLetter(userChoice)}(user) beats ${convertLetter(
+    computerChoice
+  )}(computer). You win .....!!`;
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userSpanScore.innerHTML = userScore;
+  computerSpanScore.innerHTML = computerScore;
+  result.innerHTML = `${convertLetter(
+    userChoice
+  )}(user) lose to ${convertLetter(
+    computerChoice
+  )}(computer). You lost .....!!`;
+}
+
+function draw(userChoice, computerChoice) {
+  result.innerHTML = `${convertLetter(
+    userChoice
+  )}(user) equals to ${convertLetter(
+    computerChoice
+  )}(computer). Match draw .....!!`;
+}
+
+function main() {
+  rock.addEventListener("click", () => {
+    game("rock");
+  });
+
+  paper.addEventListener("click", () => {
+    game("paper");
+  });
+
+  scissors.addEventListener("click", () => {
+    game("scissors");
+  });
+}
+
+main();
